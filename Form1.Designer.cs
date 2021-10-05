6ec651e0dd3a47186fb769b83e8d315da4f73a06
Marie Askjem <marie@getacademy.no>
2021-10-05 13:20:02 +0200

    initial checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace TheBindingOfIsaac
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.isaac = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.isaac)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // isaac
+            // 
+            this.isaac.BackgroundImage = global::TheBindingOfIsaac.Properties.Resources.isaac1;
+            this.isaac.Location = new System.Drawing.Point(135, 146);
+            this.isaac.Name = "isaac";
+            this.isaac.Size = new System.Drawing.Size(36, 42);
+            this.isaac.TabIndex = 0;
+            this.isaac.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TheBindingOfIsaac.Properties.Resources.Downpour;
+            this.ClientSize = new System.Drawing.Size(901, 601);
+            this.Controls.Add(this.isaac);
+            this.Name = "Form1";
+            this.Text = "Form1";        
+            ((System.ComponentModel.ISupportInitialize)(this.isaac)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox isaac;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheBindingOfIsaac
+{
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+   
+    public partial class Form1 : Form
+    {
+        public EventHandler NewDirectionReceived;
+        Direction currentDirection;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            currentDirection = Direction.Right;
+
+            this.KeyDown += Form1_KeyDown;
+            NewDirectionReceived += direction_received;
+
+
+            Task.Run(() => MoveTask());
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private Task MoveTask()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                NewDirectionReceived(this,new EventArgs());
+            }
+        }
+        private void direction_received(object sender, EventArgs e)
+        {
+            MoveIsaac(currentDirection);
+        }
+        //Må legge til alle retninger isaac kan gå
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                currentDirection = Direction.Up;
+        }
+
+        //Kan skrives om til en generell metode som kan flytte hvilken som helst figur
+        // i dette tilfellet flytter han på imaget som ligger på formen og heter isaac
+        private void MoveIsaac(Direction moveDirection)
+        {
+            var xLocation = isaac.Location.X;
+            var yLocation = isaac.Location.Y;
+
+            //Her må du ha en sjekk på hvilken retning man skal flytte isaac,
+            // og øke/minske x, y koordinater ut ifra det. Eksempelet under
+            // viser at isaac flytter seg til høyre ( x-aksens verdi øker)
+            isaac.PerformSafely(()=>isaac.Location = new Point(xLocation + 2, yLocation));
+          
+        }
+
+
+
+    }
+}
